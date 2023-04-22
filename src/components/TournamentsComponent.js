8ae3f05db13a7f0c086598f5a494dae7a95e552c
Olysar1 <giorgibarbakadz1@gmail.com>
2023-04-22 16:12:17 +0400

    style timing component

--- src/components/TournamentsComponent.js
+++ src/components/TournamentsComponent.js
@@ -1,8 +1,13 @@
+import clock from "../assets/clock.svg";
+
 const TournamentsComponent = () => {
   return (
     <div className="routed-container-body spring-series">
       <h2>სერიის ფარგლებში, ყოველდღიურად გაიმართება 3 ივენთი:</h2>
-      <span className="timing">19:00 / 19:30 / 20:00</span>
+      <div className="timing-container">
+        <img src={clock} />
+        <span className="timing">19:00 / 19:30 / 20:00</span>
+      </div>
       <div style={{ width: 450 }}>
         <p>* ყველა ტურნირზე მოხვედრა შესაძლებელია 0 ლარიდან.</p>
         <p>ტურნირების სრული განრიგის სანახავად გადადი პოკერის ლობიში</p>
